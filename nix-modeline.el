b832bf9429ba0f6a6a5bff247e679f8fcd275e5d
Jordan Mulcahey <snhjordy@gmail.com>
2021-04-03 04:32:51 -0700

    Replace entr with filenotify

--- nix-modeline.el
+++ nix-modeline.el
@@ -25,7 +25,7 @@
 ;;; Commentary:
 
 ;; Displays the number of running Nix evaluations in the modeline.
-;; Runs efficiently by using the `entr' command-line utility to watch
+;; Runs efficiently using the filenotify built-in library to watch
 ;; files that Nix updates on the start and completion of each
 ;; operation. Useful as the missing UI for Nix asynchronous build tools
 ;; like `lorri'.
@@ -33,6 +33,8 @@
 ;;; Code:
 
 (eval-when-compile (require 'subr-x))
+(require 'filenotify)
+(require 'seq)
 
 (defgroup nix-modeline nil
   "Display running Nix builders in the modeline."
@@ -40,7 +42,7 @@
   :group 'tools)
 
 (defcustom nix-modeline-default-text " λ(?) "
-  "The text for nix-modeline to display before its process starts."
+  "The text for nix-modeline to display before its watchers start."
   :type 'string)
 
 (defcustom nix-modeline-idle-text " λ(✓) "
@@ -55,18 +57,11 @@ of Nix builders running."
   :type 'string)
 
 (defcustom nix-modeline-error-text " λ(X) "
-  "The text to display when nix-modeline's process crashes."
+  "The text to display when nix-modeline encounters an error."
   :type 'string)
 
-(defcustom nix-modeline-entr-command "entr"
-  "The path to the entr command line utility.
-
-Note that Nix substituteInPlace will edit this file to a correct path if
-nix-modeline is built as a declarative elisp package."
-  :type '(file :must-match t))
-
-(defcustom nix-modeline-trigger-files '("/nix/var/nix/db/db.sqlite")
-  "A list of files nix-modeline will monitor.
+(defcustom nix-modeline-trigger-files '("/nix/var/nix/db")
+  "A list of files and directories nix-modeline will monitor.
 
 Changes to any of these files will cause nix-modeline to update, which forces a
 modeline redisplay. The ideal files for this purpose should reliably be changed
@@ -78,9 +73,9 @@ only when Nix operations begin and end."
 
 Usually, setting this variable only makes sense in multi-user Nix environments.
 
-'self means to track only your own builders.
-'self-and-root means your builders and those belonging to root get tracked.
-'all means to track all of the builders on the system."
+`self' means to track only your own builders.
+`self-and-root' means your builders and those belonging to root get tracked.
+`all' means to track all of the builders on the system."
   :type '(choice (const :tag "Your User" 'self)
                  (const :tag "Your User and root" 'self-and-root)
                  (const :tag "All Users" 'all)))
@@ -88,25 +83,34 @@ Usually, setting this variable only makes sense in multi-user Nix environments.
 (defcustom nix-modeline-process-regex "(nix-build)|(nix-instantiate)"
   "A regex of process names that should count as Nix builders.
 
-nix-modeline passes this regex to pgrep and uses the number of matching
-processes to report how many Nix builders are in progress."
+nix-modeline  uses the number of matching processes to report how many Nix
+builders are in progress."
   :type 'regexp)
 
-(defcustom nix-modeline-pgrep-string (pcase system-type
-                                       ('darwin "pgrep %s '%s' | wc -l")
-                                       (_ "pgrep %s -c '%s'"))
-  "The pgrep command line that nix-modeline should use.
+(defcustom nix-modeline-process-counter (pcase system-type
+                                          ('darwin 'pgrep)
+                                          (_ 'lisp))
+  "The method nix-modeline should use to count Nix builders.
+
+`lisp' is a pure Emacs Lisp process counter with no runtime dependencies.
+`pgrep' starts a pgrep process on every nix-modeline event. This is useful on
+platforms like macOS where `process-attributes' returns nothing useful."
+  :type '(choice (const :tag "Pure Lisp" 'lisp)
+                 (const :tag "pgrep" 'pgrep)))
+
+(defcustom nix-modeline-pgrep-string "pgrep %s %s"
+  "The command line used when counting processes with pgrep.
 
 Note: the first %s in this variable gets replaced by the value of
 `nix-modeline--pgrep-users', and the second %s gets replaced by the value of
 `nix-modeline-process-regex'."
   :type 'string)
 
-(defcustom nix-modeline-delay 0.025
+(defcustom nix-modeline-delay 0.1
   "The delay between when nix-modeline triggers and when it updates.
 
-This value is in seconds. Short (microsecond) delays help prevent race
-conditions in nix-modeline during lengthy Nix builds."
+This value is in seconds. Short (microsecond) delays help coalesce file watcher
+events, and prevent race conditions in nix-modeline during lengthy Nix builds."
   :type 'number)
 
 (defcustom nix-modeline-hook nil
@@ -123,13 +127,22 @@ conditions in nix-modeline during lengthy Nix builds."
 
 (defface nix-modeline-error-face
   '((t :inherit warning))
-  "Face used when nix-modeline's process crashes.")
+  "Face used when nix-modeline's encounters an error.")
 
-(defvar nix-modeline--process nil
-  "The process nix-modeline uses to monitor Nix build processes.")
+(defvar nix-modeline--watchers '()
+  "The list of file watchers nix-modeline uses to monitor Nix.")
+
+(defvar nix-modeline--timer nil
+  "The timer configured by `nix-modeline-delay'.")
 
 (defvar nix-modeline--status-text ""
   "The string representing the current Nix builder status.")
+
+(defun nix-modeline--error (msg)
+  "Set nix-modeline to the error state and print MSG."
+  (setq nix-modeline--status-text (propertize nix-modeline-error-text
+                                              'face 'nix-modeline-error-face))
+  (message (format "nix-modeline: %s" msg)))
 
 (defun nix-modeline--update (num-builders)
   "Update nix-modeline's text and force redisplay all modelines.
@@ -144,73 +157,98 @@ number of Nix builder processes it saw running."
   (run-hooks 'nix-modeline-hook)
   (force-mode-line-update 'all))
 
-(defun nix-modeline--callback (process output)
-  "Update nix-modeline based on the OUTPUT of its PROCESS."
+(defun nix-modeline--pgrep-filter (process output)
+  "Update nix-modeline based on the OUTPUT of its pgrep PROCESS."
   (ignore process)
-  (dolist (num-builders (split-string output nil 'omit-nulls))
-    (nix-modeline--update (string-to-number num-builders))))
+  (let ((nix-procs (split-string output nil 'omit-nulls)))
+        (nix-modeline--update (length nix-procs))))
 
-(defun nix-modeline--sentinel (process event)
-  "Inspects EVENT, and alerts the user if nix-modeline's PROCESS crashes."
-  (ignore event)
-  (unless (process-live-p process)
-    (setq nix-modeline--status-text (propertize nix-modeline-error-text
-                                                'face 'nix-modeline-error-face))))
+(defun nix-modeline--pgrep-sentinel (process event)
+  "When the pgrep PROCESS signals an EVENT, handle it.
+
+An oddity of pgrep is that it exits with code 1 when no processes match, which
+we handle here."
+  (ignore process)
+  (pcase event
+    ("finished\n" nil)
+    ("exited abnormally with code 1\n" (nix-modeline--update 0))
+    (e (nix-modeline--error (format "pgrep %s" e)))))
 
 (defun nix-modeline--pgrep-users ()
   "Convert a nix-modeline users setting into a pgrep argument."
   (pcase nix-modeline-users
-    ('self "-U $(id -u)")
-    ('self-and-root "-U $(id -u) -U 0")
+    ('self (format "-U %s" (user-uid)))
+    ('self-and-root (format "-U %s -U 0" (user-uid)))
     ('all "")))
 
-(defun nix-modeline--start-process ()
-    "Start nix-modeline's Nix watcher process."
-    (let ((process-connection-type nil))
-      (setq nix-modeline--process (make-process
-                                   :name "Nix Process Watcher"
-                                   :buffer nil
-                                   :command
-                                   (list shell-file-name
-                                         shell-command-switch
-                                         (string-join
-                                          (list
-                                           "while [ true ]; do"
-                                           "printf" (string-join
-                                                     nix-modeline-trigger-files
-                                                     "\\n")
-                                           "|"
-                                           nix-modeline-entr-command "-dns"
-                                           (format "\"sleep %s; %s\""
-                                                   nix-modeline-delay
-                                                   (format
-                                                    nix-modeline-pgrep-string
-                                                    (nix-modeline--pgrep-users)
-                                                    nix-modeline-process-regex))
-                                                    "2>/dev/null"
-                                                    "; done") " "))
-                                   :filter 'nix-modeline--callback
-                                   :sentinel 'nix-modeline--sentinel)))
-    (set-process-query-on-exit-flag nix-modeline--process nil))
+(defun nix-modeline--pgrep-callback ()
+  "Count Nix builder processes using pgrep."
+  (let ((process-connection-type nil))
+    (make-process
+     :name "Nix Process Counter"
+     :buffer nil
+     :command (split-string (format nix-modeline-pgrep-string
+                                    (nix-modeline--pgrep-users)
+                                    nix-modeline-process-regex)
+                            nil 'omit-nulls)
+     :filter 'nix-modeline--pgrep-filter
+     :sentinel 'nix-modeline--pgrep-sentinel
+     :noquery t)))
+
+(defun nix-modeline--pure-callback ()
+  "Count Nix builder processes using pure Emacs Lisp."
+  (let ((nix-procs
+         (seq-filter (lambda (p) (and (string-match-p
+                                       nix-modeline-process-regex
+                                       (alist-get 'comm p ""))
+                                      (eq (alist-get 'euid p) (user-uid))))
+                     (mapcar (lambda (pid) (process-attributes pid))
+                             (list-system-processes)))))
+    (nix-modeline--update (length nix-procs))))
+
+(defun nix-modeline--callback ()
+  "On a watch event, select a callback implementation."
+  (pcase nix-modeline-process-counter
+    ('lisp (nix-modeline--pure-callback))
+    ('pgrep (nix-modeline--pgrep-callback))))
+
+(defun nix-modeline--start-watchers ()
+  "Start watchers for the paths in `nix-modeline-trigger-files'."
+  (setq nix-modeline--watchers
+        (mapcar (lambda (path)
+                  (and (timerp nix-modeline--timer)
+                       (cancel-timer nix-modeline--timer))
+                  (file-notify-add-watch path
+                                         '(change)
+                                         (lambda (_event)
+                                           (run-with-timer nix-modeline-delay
+                                                           nil
+                                                           #'nix-modeline--callback))))
+                nix-modeline-trigger-files)))
+
+(defun nix-modeline--stop-watchers ()
+  "Stop the watchers in `nix-modeline--watchers'."
+  (dolist (watcher nix-modeline--watchers)
+    (file-notify-rm-watch watcher)))
 
 ;;;###autoload
 (define-minor-mode nix-modeline-mode
   "Displays the number of running Nix builders in the modeline."
   :global t :group 'nix-modeline
-  (and nix-modeline--process
-       (process-live-p nix-modeline--process)
-       (delete-process nix-modeline--process))
+  (nix-modeline--stop-watchers)
   (setq nix-modeline--status-text "")
   (or global-mode-string (setq global-mode-string '("")))
   (cond
    (nix-modeline-mode
-    (add-to-list 'global-mode-string '(t (:eval nix-modeline--status-text)) 'append)
+    (add-to-list 'global-mode-string '(t (:eval nix-modeline--status-text))
+                 'append)
     (setq nix-modeline--status-text (propertize nix-modeline-default-text
                                                 'face 'nix-modeline-idle-face))
-    (nix-modeline--start-process))
+    (nix-modeline--start-watchers)
+    (nix-modeline--callback))
    (t
-    (setq global-mode-string (remove '(t (:eval nix-modeline--status-text)) global-mode-string))
-    (delete-process nix-modeline--process))))
+    (setq global-mode-string
+          (remove '(t (:eval nix-modeline--status-text)) global-mode-string)))))
 
 (provide 'nix-modeline)
 ;;; nix-modeline.el ends here
